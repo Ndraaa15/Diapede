9df070a71f60767d33121005ad693377a47e64bd
Gede Indra Adi Brata <indrabrata599@gmail.com>
2023-11-26 15:50:13 +0700

    `Update:` back end structure and logic

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,7 @@
-"""
-URL configuration for diapede project.
+"""main URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+    https://docs.djangoproject.com/en/4.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -15,10 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from diapede import views
+from django.urls import path
+from main import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
     path('diabetes/', views.analysis_diabetes),
 ]
